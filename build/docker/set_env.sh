8d96e884c46f3d91b80c5f66b50b35dc289b7bf9
airenas <airenass@gmail.com>
2021-09-15 16:42:07 +0300

    Add docker build scripts

---
+++ build/docker/set_env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo Configuring BASE_HREF to "$BASE_HREF"
+sed -i "s|<base href=\"/\">|<base href=\"$BASE_HREF\">|" /usr/share/nginx/html/index.html
+
+echo Configuring MODELS_URL to "$MODELS_URL"
+sed -i "s|/synthesis.data/models.json|$MODELS_URL|" /usr/share/nginx/html/index.html
+echo Env conf done
