d20ec9748ee92f9c098d450bc13e3dbfbe673398
airenas <airenass@gmail.com>
2021-09-21 12:45:47 +0300

    Fix problem on undefined modelURL

--- synthesis-web/src/app/config.ts
+++ synthesis-web/src/app/config.ts
@@ -16,7 +16,7 @@ export class Config {
         this.synthesisURL = serverURL + environment.synthesisURL;
         this.sayingURL = serverURL + environment.sayingURL + '/saying';
         this.modelsURL = serverURL + environment.modelsURL;
-        if (modelsURL !== '') {
+        if ((modelsURL ?? '') !== '') {
             this.modelsURL = serverURL + modelsURL;
         }
     }
