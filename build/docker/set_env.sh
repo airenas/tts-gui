6359807f6c52825813cb7da8c0fcf6eb282afc82
airenas <airenass@gmail.com>
2022-04-14 15:36:08 +0300

    Allow to set service url from env

--- build/docker/set_env.sh
+++ build/docker/set_env.sh
@@ -5,3 +5,7 @@ sed -i "s|<base href=\"/\">|<base href=\"$BASE_HREF\">|" /usr/share/nginx/html/i
 echo Configuring MODELS_URL to "$MODELS_URL"
 sed -i "s|/synthesis.data/models.json|$MODELS_URL|" /usr/share/nginx/html/index.html
 echo Env conf done
+
+echo Configuring SERVICE_URL to "$SERVICE_URL"
+sed -i "s|https://sinteze.intelektika.lt|$SERVICE_URL|" /usr/share/nginx/html/index.html
+echo Env conf done
